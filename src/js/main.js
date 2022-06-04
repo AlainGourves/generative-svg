3e2696fc94aeea322b91add602e2e773717b7d4d
Alain Gourvès <alain.gourves@gmail.com>
2022-06-04 18:17:36 +0200

    block models added

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 import { random } from "https://cdn.skypack.dev/@georgedoescode/generative-utils@1.0.38";
 
-const numRows = random(6, 8, true); // true: gives an integer
-const numCols = random(8, 12, true);
+const numRows = random(8, 10, true); // true: gives an integer
+const numCols = random(6, 8, true);
 const squareSize = 40;
+
+const btnExport = document.querySelector('button');
 
 let draw;
 let colors
@@ -12,8 +14,9 @@ const generateLittleBox = (x, y) => {
     // get 2 colors
     const { foreground, background } = getTwoColors();
 
-    // const blockStyleOptions = [drawCircle, drawOppositeTriangles, drawOppositeCircles];
-    const blockStyleOptions = [drawRect, drawOppositeCircles,drawOppositeTriangles];
+    // const blockStyleOptions = [drawDisc];
+    const blockStyleOptions = [drawRect, drawFacingCircles, drawSemiCircle, drawCircle, drawOppositeCircles, drawDisc, drawOppositeTriangles];
+    // const blockStyleOptions = [drawFacingCircles, drawSemiCircle, drawCircle, drawDisc];
     const blockStyle = random(blockStyleOptions);
     blockStyle(x * squareSize, y * squareSize, foreground, background);
 }
@@ -33,13 +36,8 @@ const getTwoColors = () => {
 const drawRect = (x, y, foreground, background) => {
     // Create group element
     const group = draw.group().addClass('draw-block');
-
     // Draw block
-    group
-        .rect(squareSize, squareSize)
-        .fill(background)
-        .stroke('none')
-        .move(x, y);
+    group.rect(squareSize, squareSize).fill(background).stroke('none').move(x, y);
 }
 
 const drawCircle = (x, y, foreground, background) => {
@@ -67,6 +65,46 @@ const drawOppositeCircles = (x, y, foreground, background) => {
     circleGroup.circle(squareSize).fill(foreground).center(x + offsets[0], y + offsets[1]); //Bottom left
     circleGroup.circle(squareSize).fill(foreground).center(x + offsets[2], y + offsets[3]); // top right
     // assign mask
+    circleGroup.maskWith(mask);
+    group.add(circleGroup);
+}
+
+const drawFacingCircles = (x, y, foreground, background) => {
+    const group = draw.group().addClass('opposite-circles');
+    // group for the circles
+    const circleGroup = draw.group();
+    group.rect(squareSize, squareSize).fill(background).move(x, y);
+    // mask
+    const mask = draw.rect(squareSize, squareSize).fill('#fff').move(x, y);
+    circleGroup.circle(squareSize).fill(foreground).center(x, y + squareSize / 2);
+    circleGroup.circle(squareSize).fill(foreground).center(x + squareSize, y + squareSize / 2);
+    circleGroup.maskWith(mask);
+    if (random(0, 1, true)) circleGroup.rotate(90)
+    group.add(circleGroup);
+}
+
+const drawSemiCircle = (x, y, foreground, background) => {
+    const group = draw.group().addClass('semi-circles');
+    group.rect(squareSize, squareSize).fill(background).move(x, y);
+    // group for the circles
+    const circleGroup = draw.group();
+    circleGroup.path(`M${x} ${y} a ${squareSize / 2} ${squareSize / 2} 0 0 1 0 ${squareSize} z`).fill(foreground);
+    circleGroup.path(`M${x + squareSize / 2} ${y} a ${squareSize / 2} ${squareSize / 2} 0 0 1 0 ${squareSize} z`).fill(foreground);
+    const dir = random(0, 3, true);
+    circleGroup.rotate(dir * 90);
+    group.add(circleGroup);
+}
+
+const drawDisc = (x, y, foreground, background) => {
+    const group = draw.group().addClass('disc');
+    // group for the circles
+    const circleGroup = draw.group();
+    group.rect(squareSize, squareSize).fill(background).move(x, y);
+    circleGroup.circle(squareSize).fill(foreground).move(x, y)
+    // mask
+    const bg = draw.rect(squareSize, squareSize).fill('#fff').move(x, y);
+    const hole = draw.circle(squareSize / 2).fill('black').center(x + squareSize / 2, y + squareSize / 2);
+    const mask = draw.mask().add(bg).add(hole);
     circleGroup.maskWith(mask);
     group.add(circleGroup);
 }
@@ -142,4 +180,20 @@ window.addEventListener("load", async e => {
         }
 
     }
+
+    btnExport.addEventListener('click', e => {
+        const toSave = draw.svg();
+        const fileDowloadUrl = URL.createObjectURL(
+            new Blob([toSave], { type: 'image/svg+xml;charset=utf-8;' })
+        );
+        const link = document.createElement('a');
+        link.href = fileDowloadUrl;
+        link.setAttribute(
+            'download',
+            `image.svg`
+        );
+        document.body.appendChild(link);
+        link.click();
+        link.parentNode.removeChild(link);
+    });
 });
